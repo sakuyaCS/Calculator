c21b072f86c3b4c4d4466c869fedbe9179dfb555
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2025-06-03 17:01:08 -0400

    Feat: implement basic math and operate functions

---
+++ script.js
@@ -0,0 +1,65 @@
+let operator;
+let a;
+let b;
+
+
+
+
+function addition (num1, num2){ 
+
+    let a = num1;
+    let b = num2;
+
+    let sum = a + b;
+
+    return sum;
+
+}
+
+function subtraction(num1, num2) {
+    let a = num1;
+    let b = num2;
+
+    let difference = a - b;
+    
+    return difference;
+}
+
+function multiply (num1, num2) {
+
+    let a = num1;
+    let b = num2;
+
+    let product = a * b;
+
+    return product;
+
+
+}
+
+function divide (num1, num2) {
+
+    let a = num1;
+    let b = num2;
+
+    let quotient = a/b;
+
+    return quotient;
+}
+
+function operate (operator, num1, num2) {
+
+    if (operator === "+") {
+        return addition(num1, num2);
+    } else if (operator === "-") { 
+        return subtraction(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    }
+
+    
+
+
+}
